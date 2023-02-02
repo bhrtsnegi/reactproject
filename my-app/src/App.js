2a8693dae7ef349e9fde8fb31ebd48a81042519d
Bharat Negi <1bhrtsnegi@gmail.com>
2023-02-02 18:40:37 +0530

    Update App.js
    File content change to original content

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,9 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-          Don't Learn
-          I will Learn
+          Write your code here
         </a>
       </header>
     </div>
